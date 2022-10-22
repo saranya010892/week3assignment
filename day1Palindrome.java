4a2637644e885749a911048bf1dcf8aad534a7a7
saranya010892 <114097530+saranya010892@users.noreply.github.com>
2022-10-22 16:44:09 +0530

    Create day1Palindrome.java

---
+++ day1Palindrome.java
@@ -0,0 +1,21 @@
+# week3assignment
+package week3day1;
+
+public class Palindrome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str="madam";
+			String rev = "";
+			 
+			 for(int i=(str.length()-1);i>=0;i--)
+			 rev = rev+str.charAt(i);
+		
+			 if(str.equals(rev))
+				 System.out.println("string is a palindrome");	 
+			 else 
+				 System.out.println("string is not a palindrome");
+
+	}
+
+}
